88853ecf351319576a134ddd2124fe930dd2757e
frandrew90 <frandrewjs@gmail.com>
2021-09-03 17:29:51 +0300

    made color switch

--- index.js
+++ index.js
@@ -8,3 +8,38 @@ const colors = [
   "#009688",
   "#795548",
 ];
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
+refs.startBtn.addEventListener("click", onStartBtn);
+refs.stopBtn.addEventListener("click", onStopBtn);
+
+const min = 0;
+const max = colors.length - 1;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+console.log(randomIntegerFromInterval(min, max));
+
+let intervalId = null;
+
+function onStartBtn() {
+  refs.startBtn.disabled = true;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(min, max)];
+  }, 1000);
+}
+
+function onStopBtn() {
+  refs.startBtn.disabled = false;
+  clearInterval(intervalId);
+}
+
+console.log(refs.startBtn);
+console.log(refs.stopBtn);
